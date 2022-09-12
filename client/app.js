c825e5b8e82dd4c2397f0f2e2a1985a50ae7c2bf
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-12 13:57:02 +0200

    API done and front done

--- client/app.js
+++ client/app.js
@@ -6,6 +6,7 @@ const getFlight = () => {
     .then(response => response.json())
     .then(flights => {
       console.log("voici les vols :", flights)
+      populateTable(flights)
     })
     .catch(err => console.log(err))
 }
@@ -13,8 +14,37 @@ const getFlight = () => {
 getFlight();
 
 const populateTable = (flights) => {
+  console.log("from populateTable :", flights)
   for (const flight of flights) {
     const tableRow = document.createElement("tr");
     const tableIcon = document.createElement("td");
+    tableIcon.textContent = "🛫";
+    tableRow.append(tableIcon);
+
+    const flightDetails = {
+      time: flight.departing.slice(0, 5),
+      destination: flight.destination.toUpperCase(),
+      flight: flight.flightNumber.shift(),
+      gate: flight.gate,
+      remarks: flight.status.toUpperCase()
+    }
+
+    for (const flightDetail in flightDetails) {
+      const tableCell = document.createElement("td");
+      const word = Array.from(flightDetails[flightDetail])
+
+      for (const [index, letter] of word.entries()) {
+        const letterElement = document.createElement("div");
+
+        setTimeout(() => {
+          letterElement.classList.add("flip")
+          letterElement.textContent = letter;
+          tableCell.append(letterElement)
+        }, 100 * index);
+      }
+      tableRow.append(tableCell);
+    }
+
+    tableBody.append(tableRow);
   }
 }
