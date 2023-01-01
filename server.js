ef5d357131e91d562ad7c0623f71e3ace421dd67
Vincent BERNIER <vincent.bernier@hotmail.fr>
2023-01-01 14:42:55 +0100

    try to debug but it's an error on the server API - email sent to support

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-const PORT = 8000
+const PORT = 8000;
 const express = require("express");
-const axios = require("axios");
+const axios = require("axios").default;
 const app = express();
 const cors = require("cors");
 
 require("dotenv").config();
 
-
 app.use(cors());
 
+app.get("/flights", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://toronto-pearson-airport.p.rapidapi.com/arrivals/terminal/3",
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "toronto-pearson-airport.p.rapidapi.com",
+    },
+  };
 
-app.get('/flights', (req, res) => {
-const options = {
-  method: 'GET',
-  url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
-  headers: {
-    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-    'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-
-  res.json(response.data.slice(0,6))
-}).catch(function (error) {
-	console.error(error);
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log("********** coucou *************************");
+      console.error(error);
+    });
 });
-});
 
-
-
-app.listen(PORT, () => console.log(`running on PORT: http://localhost:${PORT}/flights`));
+app.listen(PORT, () =>
+  console.log(`running on PORT: http://localhost:${PORT}/flights`)
+);
